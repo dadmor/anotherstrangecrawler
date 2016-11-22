33ed7fe788b96c6d8fa192d49eebdbcb75a43208
gdurtan <you@example.com>
2016-11-22 21:34:41 +0100

    change readme

--- inc/mongo-export-csv.js
+++ inc/mongo-export-csv.js
@@ -1,7 +1,7 @@
-var query=null;
-var query = { pageInsideCms : null }
-var query = { metaGeneratorTest: /.*WordPress 2.*/ }
-var query = { metaGeneratorTest : null }
+var query = null;
+//var query = { pageInsideCms : null }
+//var query = { metaGeneratorTest: /.*WordPress 2.*/ }
+//var query = { metaGeneratorTest : null }
 
 
 db.MyCollection.find(query).forEach(function(MyCollection){
